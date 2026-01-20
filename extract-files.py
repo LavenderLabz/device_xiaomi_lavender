945e5f46c3ca5959cfb3a4b85a4564dbe0fc5fe6
pix106 <sbordenave@gmail.com>
2026-01-20 17:50:04 +0100

    sdm660-common: Drop libaudioclient_shim
    * not needed anymore

--- extract-files.py
+++ extract-files.py
@@ -122,7 +122,6 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib64/hw/consumerir.lirc.sdm660.so', 'consumerir.spi.sdm660.so'): blob_fixup()
         .fix_soname(),
     'system_ext/lib64/libqxrsplitauxservice.qti.so': blob_fixup()
-        .add_needed('libaudioclient_shim.so')
         .add_needed('libwfdservice_shim.so')
 }  # fmt: skip
 
